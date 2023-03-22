da8c0b1b31422dea7174c2bc56816a9e98e9d256
sukanya2712 <sukanyanaik511@gmail.com>
2023-03-22 23:37:21 +0530

    ADDED_UC2

--- Stack.java
+++ Stack.java
@@ -2,6 +2,7 @@ package com.stackimplementation;
 
 
 import com.LinkedListImplementation.LinkedList;
+import com.LinkedListImplementation.Node;
 
 public class Stack<T extends Comparable<T>> {
     //UC1
@@ -14,4 +15,19 @@ public class Stack<T extends Comparable<T>> {
     public void show() {
         linkedList.show();
     }
+
+
+    public T pop() {
+        return linkedList.pop();
+    }
+
+    //uc2
+    public void peek() {
+        Integer peeked = (Integer) linkedList.pop();
+        System.out.println("The peek element is " + peeked);
+        linkedList.push((T) peeked);
+    }
+
+
+
 }
--- StackMain.java
+++ StackMain.java
@@ -8,6 +8,26 @@ public class StackMain {
         stack.push(70);
         stack.push(30);
         stack.push(56);
+        System.out.println("----------CREATED STACK---------- ");
         stack.show();
+
+        //uc2
+        System.out.println("");
+        System.out.println("----------PEEK ELEMENT-----------");
+        stack.peek();
+        System.out.println("");
+
+        System.out.println("---------POPPING ELEMENT TILL STACK IS EMPTY-------");
+        Integer poppedData;
+        do {
+            poppedData = stack.pop();
+            if (poppedData == null)
+                System.out.println("Stack is Empty");
+            else
+                System.out.println("The element popped is => " + poppedData);
+            stack.show();
+        } while( poppedData != null);
+
+
     }
 }
